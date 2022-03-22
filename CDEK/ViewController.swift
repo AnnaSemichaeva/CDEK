5d49ac6a7a097ea14c2893deb6302e3c442f6f75
AnnaSemichaeva <tdhnss@gmail.com>
2022-03-22 22:22:00 +0300

    logic for the first screen

--- CDEK/ViewController.swift
+++ CDEK/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hightSlider: UISlider!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func result(_ sender: UIButton) {
+        let height = hightSlider.value
+        let weight = weightSlider.value
+        
+        print(height)
+        print(weight)
+        
+    }
+    
 }
 
