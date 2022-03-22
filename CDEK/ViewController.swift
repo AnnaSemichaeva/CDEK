941328b012fea7de829d9bf548f4be7d83d0d4da
AnnaSemichaeva <tdhnss@gmail.com>
2022-03-22 22:52:43 +0300

    changes to UI for stack view

--- CDEK/ViewController.swift
+++ CDEK/ViewController.swift
@@ -12,13 +12,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var hightSlider: UISlider!
     @IBOutlet weak var hightLabel: UILabel!
     @IBAction func hightValue(_ sender: UISlider) {
-        hightLabel.text = String(format: " %.1f", sender.value)
+        hightLabel.text = String(format: " %.1f", sender.value) + String(" см")
     }
     
     @IBOutlet weak var weightSlider: UISlider!
     @IBOutlet weak var weightLabel: UILabel!
     @IBAction func weightValue(_ sender: UISlider) {
-        weightLabel.text = String(format: " %.1f", sender.value)
+        weightLabel.text = String(format: " %.1f", sender.value) + String(" см")
     }
     
     override func viewDidLoad() {
